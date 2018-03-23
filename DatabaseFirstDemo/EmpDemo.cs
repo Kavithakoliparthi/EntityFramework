05638fd80636a0c22efb461c3405cf206544f085
Kavithakoliparthi <koliparthik@gmail.com>
2018-03-23 19:43:54 +0530

    EntityFramework

---
+++ DatabaseFirstDemo/EmpDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class EmpDemo
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities();
+            var emp = new Employee()
+            {
+                EID = 2,
+                EName = "srinu"
+
+            };
+            context.Employees.Add(emp);
+            context.SaveChanges();
+        }
+
+    }
+}
---
+++ DatabaseFirstDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities();
+            var emp = new Employee()
+            {
+                EID=1,
+                EName="anusha"
+               
+            };
+            context.Employees.Add(emp);
+            context.SaveChanges();
+        }
+    }
+}
